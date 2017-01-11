0e4ef8e5fa0b28bb52c660c96d31edafee695472
Derek Yeager <dcy2003@gmail.com>
2017-01-11 16:16:15 -0500

    updated

--- launch.sh
+++ launch.sh
@@ -2,5 +2,8 @@
 
 aws cloudformation create-stack \
 	--stack-name UserDataTestStack \
-    --template-body file://cf.json \
-    --parameters ParameterKey=UserData,ParameterValue=`cat cloud-init.sh | base64`
+    --template-body file://cloudformation.json \
+    --parameters ParameterKey=UserData,ParameterValue=`cat cloud-init.sh | base64` \
+    ParameterKey=AmiID,ParameterValue=ami-38cd975d \
+    ParameterKey=InstanceName,ParameterValue=TempTestInstance \
+    ParameterKey=SecurityGroupId,ParameterValue=sg-4ecb7c27
